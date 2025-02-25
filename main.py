29dea3c2c07c9d18e1342d76ae4b32ba7f83e372
Jeff Ferr <jeffersonfr@gmail.com>
2025-02-25 15:09:09 -0300

    Update 'localhost' to 'redis' in main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # Configuração do Redis
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 # Pasta onde as imagens estão armazenadas
 IMAGE_FOLDER = 'images'
